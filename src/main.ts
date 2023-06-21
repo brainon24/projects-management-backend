4ff4a0f4b77a38c52f6affb53b3fe88b0ec35eee
daviddiazh <daviddiazlds1221@gmail.com>
2023-06-20 22:04:03 -0500

    new changes of cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
+    origin: ['https://portal-brainon24.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
