aca43ecdbba2fa2f103a0f5eff956dd84ee9cdd4
daviddiazh <daviddiazlds1221@gmail.com>
2023-06-20 22:56:58 -0500

    new changes

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './application/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
 
   // app.setGlobalPrefix('api');
-  app.enableCors();
+  // app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
