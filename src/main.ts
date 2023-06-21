849e803dc7b5a0f61da77f3eef4ff9916e15a1b6
daviddiazh <daviddiazlds1221@gmail.com>
2023-06-20 21:57:41 -0500

    new settings to cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
